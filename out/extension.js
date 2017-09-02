1fa24b16669c52db1f0a74b4bc89d13a2004e1db
Armitxes <4rmitxes@gmail.com>
2017-09-02 14:20:35 +0200

    VSCE Compile results

--- out/extension.js
+++ out/extension.js
@@ -1,20 +1,20 @@
 'use strict';
-exports.__esModule = true;
-var vscode_1 = require("vscode");
-var vscode_languageclient_1 = require("vscode-languageclient");
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode_1 = require("vscode");
+const vscode_languageclient_1 = require("vscode-languageclient");
 function activate(context) {
     // The server is implemented in node
-    var serverModule = context.asAbsolutePath('server/init.js');
+    let serverModule = context.asAbsolutePath('server/init.js');
     // The debug options for the server
-    var debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
+    let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
-    var serverOptions = {
+    let serverOptions = {
         run: { module: serverModule, transport: vscode_languageclient_1.TransportKind.ipc },
         debug: { module: serverModule, transport: vscode_languageclient_1.TransportKind.ipc, options: debugOptions }
     };
     // Options to control the language client
-    var clientOptions = {
+    let clientOptions = {
         // Register the server for plain text documents
         documentSelector: ['sqf'],
         synchronize: {
@@ -23,11 +23,11 @@ function activate(context) {
         }
     };
     // Create the language client and start the client.
-    var lc = new vscode_languageclient_1.LanguageClient('sqfLanguageServer', 'SQF Language Server', serverOptions, clientOptions);
-    var disposable = lc.start();
-    lc.onReady().then(function () {
-        lc.onRequest('requestRestart', function (params) {
-            vscode_1.window.showInformationMessage(params, 'Reload').then(function (selected) {
+    let lc = new vscode_languageclient_1.LanguageClient('sqfLanguageServer', 'SQF Language Server', serverOptions, clientOptions);
+    let disposable = lc.start();
+    lc.onReady().then(() => {
+        lc.onRequest('requestRestart', (params) => {
+            vscode_1.window.showInformationMessage(params, 'Reload').then(selected => {
                 if (selected === 'Reload') {
                     vscode_1.commands.executeCommand('workbench.action.reloadWindow');
                 }
@@ -37,3 +37,4 @@ function activate(context) {
     });
 }
 exports.activate = activate;
+//# sourceMappingURL=extension.js.map
