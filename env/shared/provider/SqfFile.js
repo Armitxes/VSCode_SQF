aa32b74653ae0df718d864b0d96ffe1d1d60e54e
Jan Brodersen (Armitxes) <4rmitxes@gmail.com>
2018-11-27 17:29:45 +0100

    [IMP] Progress on file parsing

--- env/shared/provider/SqfFile.js
+++ env/shared/provider/SqfFile.js
@@ -20,13 +20,13 @@ class SqfFile {
 	}
 
     update() {
-		if (this.fileUri != null) {
+        if (this.fileUri != null) {
             this.fileObject = this.sqfProject.documents.get(this.fileUri);
             if (this.fileObject != null) {
                 this.fileContent = this.fileObject.getText();
                 this.parseFile();
             }
-		}
+        }
     }
 
     parseFile() {
@@ -35,15 +35,35 @@ class SqfFile {
 
         for (var i = 0; i < lines.length; i++) {
             let line = lines[i];
-            let lineIssues = this.validateFileLine(i, line);
 
+            // Console Issues
+            let lineIssues = this.validateFileLine(i, line);
             if (lineIssues.length > 0) { this.fileIssues = this.fileIssues.concat(lineIssues); }
 
+            let sqfFileVars = line.match(/([_A-Za-z0-9]+)(\s*)=/g);
+            if (sqfFileVars != null) {
+                for (var j = 0; j < sqfFileVars.length; j++) {
+                    let sqfFileArr = (/([_A-Za-z0-9]+)(\s*)=/g).exec(sqfFileVars[j]);
+                    if (sqfFileArr.length > 1) {
+                        let sqfFileVar = sqfFileArr[1];
+                        let c_start = line.indexOf(sqfFileVar);
+
+                        // ToDo: sqfVariable class
+                        this.fileVariables[sqfFileVar] = {
+                            line: i+1,
+                            column_start: c_start,
+                            column_end: c_start + sqfFileVar.length
+                        };
+                    }
+                }
+            }
+
+            
             this.fileLines[i] = {
                 'content': line
             };
         }
-
+        this.sqfProject.connection.console.log(Object.keys(this.fileVariables));
         this.sqfProject.connection.sendDiagnostics({uri: this.fileObject.uri, diagnostics: this.fileIssues});
     }
 
