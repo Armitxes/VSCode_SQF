af80ed21bdb64628f3c0d56d313698d18773a00f
Chase <29675446+theace0296@users.noreply.github.com>
2021-11-15 21:36:44 -0500

    Sort game versions in JSON

--- devHelper/getCommands.js
+++ devHelper/getCommands.js
@@ -15,26 +15,39 @@ const path = require('path');
     arma3: '/wiki/Category:Introduced_with_Arma_3',
   };
 
-  await Promise.all(Object.keys(introducedInGamesMap).map(async game => {
-    const html = await (await fetch(`${WIKI_BASE_URL}${introducedInGamesMap[game]}`)).text();
-    const document = parse(html);
-    const introducedInVersionMap = Array.from(document.querySelectorAll('.CategoryTreeLabel')).reduce(
-      (acc, a) => ({ ...acc, [/\d+\.\d+/.exec(a.textContent).shift()]: a.getAttribute('href') }),
-      {}
-    );
-    const gameCommands = { docs: `${WIKI_BASE_URL}${introducedInGamesMap[game]}` };
-    await Promise.all(Object.keys(introducedInVersionMap).map(async version => {
-      const cmdhtml = await (await fetch(`${WIKI_BASE_URL}${introducedInVersionMap[version]}`)).text();
-      const cmdDocument = parse(cmdhtml);
-      const commands = Array.from(cmdDocument.querySelectorAll('div.mw-category-group > ul > li > a[title]'))
-        .filter(a => !a.classList.length && !a.textContent.includes(':'))
-        .map(a => a.textContent.replace(/[ ]+/g, '_'));
-      gameCommands[version] = commands;
-    }));
-    fs.writeFileSync(
-      path.join(process.cwd(), 'devHelper/commands', `${game}.json`),
-      JSON.stringify(gameCommands, null, 2),
-      'utf8'
-    );
-  }));
+  await Promise.all(
+    Object.keys(introducedInGamesMap).map(async game => {
+      const html = await (await fetch(`${WIKI_BASE_URL}${introducedInGamesMap[game]}`)).text();
+      const document = parse(html);
+      const introducedInVersionMap = Array.from(document.querySelectorAll('.CategoryTreeLabel')).reduce(
+        (acc, a) => ({ ...acc, [/\d+\.\d+/.exec(a.textContent).shift()]: a.getAttribute('href') }),
+        {}
+      );
+      const gameCommands = { docs: `${WIKI_BASE_URL}${introducedInGamesMap[game]}` };
+      await Promise.all(
+        Object.keys(introducedInVersionMap).map(async version => {
+          const cmdhtml = await (await fetch(`${WIKI_BASE_URL}${introducedInVersionMap[version]}`)).text();
+          const cmdDocument = parse(cmdhtml);
+          const commands = Array.from(cmdDocument.querySelectorAll('div.mw-category-group > ul > li > a[title]'))
+            .filter(a => !a.classList.length && !a.textContent.includes(':'))
+            .map(a => a.textContent.replace(/[ ]+/g, '_'));
+          gameCommands[version] = commands;
+        })
+      );
+      const gameCommandsSorted = Object.keys(gameCommands)
+        .sort((a, b) => {
+          if (a === 'docs') {
+            return -1;
+          }
+          if (b === 'docs') {
+            return 1;
+          }
+          const [, aVer] = /(\d+\.\d+)/.exec(a);
+          const [, bVer] = /(\d+\.\d+)/.exec(b);
+          return Number.parseFloat(aVer) - Number.parseFloat(bVer);
+        })
+        .reduce((acc, key) => (gameCommands[key].legnth ? { ...acc, [key]: gameCommands[key] } : acc), {});
+      fs.writeFileSync(path.join(process.cwd(), 'devHelper/commands', `${game}.json`), JSON.stringify(gameCommandsSorted, null, 2), 'utf8');
+    })
+  );
 })();
