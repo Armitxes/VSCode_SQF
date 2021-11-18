cc62ae7a24db58b8d63e63df0b02066046301acc
Chase <29675446+theace0296@users.noreply.github.com>
2021-11-18 18:51:05 -0500

    Use wiki api rather than web crawling

--- devHelper/getCommands.js
+++ devHelper/getCommands.js
@@ -5,6 +5,14 @@ const path = require('path');
 
 (async () => {
   const WIKI_BASE_URL = 'https://community.bistudio.com';
+  const getApiUrl = params => `${WIKI_BASE_URL}/wikidata/api.php?${params}`;
+  const getUrlParams = category =>
+    new URLSearchParams({
+      action: 'categorytree',
+      format: 'json',
+      options: JSON.stringify({ depth: 1 }),
+      category,
+    });
   const introducedInGamesMap = {
     ofp: '/wiki/Category:Introduced_with_Operation_Flashpoint',
     ofpResistance: '/wiki/Category:Introduced_with_Operation_Flashpoint:_Elite',
@@ -14,23 +22,64 @@ const path = require('path');
     arma2oa: '/wiki/Category:Introduced_with_Arma_2:_Operation_Arrowhead',
     arma3: '/wiki/Category:Introduced_with_Arma_3',
   };
+  const introducedInGamesParamsMap = {
+    ofp: getUrlParams('Category:Introduced_with_Operation_Flashpoint'),
+    ofpResistance: getUrlParams('Category:Introduced_with_Operation_Flashpoint:_Elite'),
+    toh: getUrlParams('Category:Introduced_with_Take_On_Helicopters'),
+    arma: getUrlParams('Category:Introduced_with_Armed_Assault'),
+    arma2: getUrlParams('Category:Introduced_with_Arma_2'),
+    arma2oa: getUrlParams('Category:Introduced_with_Arma_2:_Operation_Arrowhead'),
+    arma3: getUrlParams('Category:Introduced_with_Arma_3'),
+  };
 
   await Promise.all(
-    Object.keys(introducedInGamesMap).map(async game => {
-      const html = await (await fetch(`${WIKI_BASE_URL}${introducedInGamesMap[game]}`)).text();
+    Object.keys(introducedInGamesParamsMap).map(async game => {
+      const getVersionUrlParams = title =>
+        new URLSearchParams({
+          action: 'query',
+          format: 'json',
+          list: 'categorymembers',
+          cmtitle: title,
+          cmprop: 'ids|title|type',
+          cmtype: 'page|subcat|file',
+          cmlimit: '500',
+        });
+      const html = (await (await fetch(getApiUrl(introducedInGamesParamsMap[game]))).json()).categorytree['*'];
       const document = parse(html);
-      const introducedInVersionMap = Array.from(document.querySelectorAll('.CategoryTreeLabel')).reduce(
-        (acc, a) => ({ ...acc, [/\d+\.\d+/.exec(a.textContent).shift()]: a.getAttribute('href') }),
+      const introducedInVersionMap = Array.from(document.querySelectorAll('a.CategoryTreeLabel')).reduce(
+        (acc, a) => ({
+          ...acc,
+          [/\d+\.\d+/.exec(a.textContent).shift()]: {
+            title: a.getAttribute('href').split('/wiki/').pop(),
+            params: getVersionUrlParams(a.getAttribute('href').split('/wiki/').pop()),
+          },
+        }),
         {}
       );
+
       const gameCommands = { docs: `${WIKI_BASE_URL}${introducedInGamesMap[game]}` };
       await Promise.all(
         Object.keys(introducedInVersionMap).map(async version => {
-          const cmdhtml = await (await fetch(`${WIKI_BASE_URL}${introducedInVersionMap[version]}`)).text();
-          const cmdDocument = parse(cmdhtml);
-          const commands = Array.from(cmdDocument.querySelectorAll('div.mw-category-group > ul > li > a[title]'))
-            .filter(a => !a.classList.length && !a.textContent.includes(':'))
-            .map(a => a.textContent.replace(/[ ]+/g, '_'));
+          const cmdData = await (await fetch(getApiUrl(introducedInVersionMap[version].params))).json();
+          let categoryMembers = cmdData.query.categorymembers;
+          let cmdContinue = cmdData.continue;
+          while (cmdContinue) {
+            const continueParams = new URLSearchParams({
+              action: 'query',
+              format: 'json',
+              list: 'categorymembers',
+              cmtitle: introducedInVersionMap[version].title,
+              cmprop: 'ids|title|type',
+              cmtype: 'page|subcat|file',
+              cmlimit: '500',
+              ...cmdContinue,
+            });
+            const next = await (await fetch(getApiUrl(continueParams))).json();
+            categoryMembers = [...categoryMembers, ...next.query.categorymembers];
+            cmdContinue = next.continue;
+          }
+
+          const commands = categoryMembers.filter(cat => cat.type === 'page' && !cat.title.includes(':')).map(cat => cat.title.replace(/[ ]+/g, '_'));
           gameCommands[version] = commands;
         })
       );
@@ -46,7 +95,8 @@ const path = require('path');
           const [, bVer] = /(\d+\.\d+)/.exec(b);
           return Number.parseFloat(aVer) - Number.parseFloat(bVer);
         })
-        .reduce((acc, key) => (gameCommands[key].legnth ? { ...acc, [key]: gameCommands[key] } : acc), {});
+        .reduce((acc, key) => (gameCommands[key].length ? { ...acc, [key]: gameCommands[key] } : acc), {});
+
       fs.writeFileSync(path.join(process.cwd(), 'devHelper/commands', `${game}.json`), JSON.stringify(gameCommandsSorted, null, 2), 'utf8');
     })
   );
